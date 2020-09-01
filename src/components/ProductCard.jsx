dc859ea516999c74aa53592eed9af6340dc8ce8d
Dmitrijv <dmitrij.vel@live.se>
2020-09-01 23:30:19 +0200

    cosmetic refactor

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,11 +5,6 @@ export default function ProductCard({ product }) {
   const gallery = product.images ? product.images : [];
   const productCoverSource = gallery[0] ? gallery[0].src.small : `https://via.placeholder.com/700x200`;
   const productCoverAlt = gallery[0] ? gallery[0].alt : `Cover image for this product.`;
-  /*
-    price: 3426
-    rating: 3.5
-    stock: 200
-  */
   return (
     <div className="productCard">
       <Link to={`/product/123`}>
