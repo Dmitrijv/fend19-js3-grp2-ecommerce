eac76186d40d6852520b13875c656195d74d457e
Dmitrijv <dmitrij.vel@live.se>
2020-09-01 23:40:17 +0200

    added css for a generic centered container

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function AboutPage() {
   return (
-    <div>
-      <p>this is an about page to demonstrate how header links work</p>
-      <p>check out your browser url</p>
+    <div className="centered-container">
+      <div className="white-card">
+        <p>this is an about page to demonstrate how header links work</p>
+        <p>check out your browser url</p>
+      </div>
     </div>
   );
 }
