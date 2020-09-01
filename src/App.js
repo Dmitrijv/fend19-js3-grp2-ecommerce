5d41a5e099622234afabd28e764266f75b0b228a
Dmitrijv <dmitrij.vel@live.se>
2020-09-01 14:38:00 +0200

    added a demonstration of how header links work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "./App.scss";
+import { Route, Switch } from "react-router-dom";
+
 import LayoutSimple from "./pages/LayoutSimple";
 import StartPage from "./pages/StartPage";
-import { Route, Switch } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route path={["/about"]}>
+          <LayoutSimple mainContent={<AboutPage />} />
+        </Route>
         <Route path={["/shop", "/"]}>
           <LayoutSimple mainContent={<StartPage />} />
         </Route>
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AboutPage() {
+  return <div>this is an about page to demonstrate how header links work</div>;
+}
