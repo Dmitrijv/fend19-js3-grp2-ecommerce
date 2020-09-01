deccbe7389e9717cbca9ff6a36d4babee726ba77
Dmitrijv <dmitrij.vel@live.se>
2020-09-01 13:55:29 +0200

    start page now renders layoutsimple component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
+import LayoutSimple from "./pages/LayoutSimple";
+import StartPage from "./pages/StartPage";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Switch>
+        <Route path={["/home", "/home", "/"]}>
+          <LayoutSimple mainContent={<StartPage />} />
+        </Route>
+      </Switch>
     </div>
   );
 }
