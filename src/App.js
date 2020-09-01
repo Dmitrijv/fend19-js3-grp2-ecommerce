89f101ff30e9c25e3d93d10a7923404fda46dea8
Dmitrijv <dmitrij.vel@live.se>
2020-09-01 14:01:03 +0200

    home page can be opened by two urls

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path={["/home", "/home", "/"]}>
+        <Route path={["/shop", "/"]}>
           <LayoutSimple mainContent={<StartPage />} />
         </Route>
       </Switch>
