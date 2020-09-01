929e0b7341ddd912421987a455e9ebc39dea8219
Dmitrijv <dmitrij.vel@live.se>
2020-09-01 14:36:18 +0200

    header nav now uses react link system

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="header">
       <h1>JS3 - Grupp 2 - E-commerce</h1>
-      <h2>[cart icon placeholder]</h2>
+      <h2>[cart icon component here]</h2>
       <nav>
         <ul>
           <li>
-            <a href="./" class="active">
+            <Link to={`/shop`} className="active">
               Shop
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="./">About</a>
+            <Link to={`/about`}>About</Link>
           </li>
         </ul>
       </nav>
