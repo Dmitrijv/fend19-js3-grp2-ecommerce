b8170e7f0dd092c60311cb96d706fc29a117cccd
Christopher Carleson <christopher.carleson@gmail.com>
2020-09-01 16:14:52 +0200

    fetch in app.js and DetailPage.jsx

--- src/App.js
+++ src/App.js
@@ -8,6 +8,29 @@ import AboutPage from "./pages/AboutPage";
 import DetailPage from "./pages/DetailPage";
 
 function App() {
+  let [products, setProducts] = useState([])
+  
+  function getAPI() {
+    
+    fetch('https://mock-data-api.firebaseio.com/e-commerce/products.json')
+    .then(resp => resp.json())
+    .then(response => {
+
+      // console.log(Object.keys(response))
+      // console.log(Object.entries(response))
+
+      setProducts(response)
+    })
+  }
+  
+  
+  useEffect(() => {
+    getAPI()
+    console.log(products);
+    }, [])
+  
+
+
   return (
     <div className="App">
       <Switch>
