fb00fcea4ea38fef00a66b9f9d8660a00ddc0aa3
Dmitrijv <dmitrij.vel@live.se>
2020-09-02 00:29:57 +0200

    misc tweaks to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,24 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div className="header">
-      <h1>JS3 - Grupp 2 - E-commerce</h1>
-      <h2>[cart icon component here]</h2>
-      <nav>
-        <ul>
-          <li>
-            <Link to={`/shop`} className="active">
-              Shop
-            </Link>
-          </li>
-          <li>
-            <Link to={`/about`}>About</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="centered-container">
+        <h1>JS3 - Grupp 2 - E-commerce</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={`/shop`} className="active">
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link to={`/about`}>About</Link>
+            </li>
+            <li>
+              <Link to={`/cart`}>Cart (0)</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
