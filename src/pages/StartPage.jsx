3805af1aa0f557c16cb822a3bca3c7ca7559e827
Dmitrijv <dmitrij.vel@live.se>
2020-09-02 01:38:26 +0200

    start page now uses npm library Masonry to draw product grid, make sure to run npm install

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import ProductCard from "../components/ProductCard";
+import Masonry from "react-masonry-css";
 
 export default function StartPage(props) {
   const products = props.products;
 
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 4,
+    700: 3,
+    500: 1
+  };
+
   return (
     <div className="centered-container">
-      {products &&
-        Object.entries(products).map((product, index) => {
-          // console.log(product);
-          return <ProductCard key={`product-card-${index}`} product={product[1]} />;
-        })}
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {products &&
+          Object.entries(products).map((product, index) => {
+            // console.log(product);
+            return <ProductCard key={`product-card-${index}`} product={product[1]} />;
+          })}
+      </Masonry>
     </div>
   );
 }
