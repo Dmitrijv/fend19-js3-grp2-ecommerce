c8d06ae64930e4723d54c0914035be515885b810
Maria Hannesdottir <mariaberghannesdottir@gmail.com>
2020-09-02 12:52:26 +0200

    started work on cart confirm

--- src/components/CartConfirm.jsx
+++ src/components/CartConfirm.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
 export default function CartConfirm() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const firstnameInput = useRef();
+  const lastnameInput = useRef();
+  const URL = "google.com";
+
+  const orderData = {
+    "group-2": {
+      firstname: firstnameInput.current.value,
+      lastname: lastnameInput.current.value,
+      //hämta från useContext: carten
+      //ordered_products: [item],
+      // total: price,
+    },
+  };
+
+  function sendOrderToAPI() {
+    //fetch
+    fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(orderData),
+    });
+  }
+
+  return (
+    <div>
+      <input ref={firstnameInput} type="text">
+        First name:
+      </input>
+      <input ref={lastnameInput} type="text">
+        Lastname:
+      </input>
+      <button onClick={sendOrderToAPI}>Confirm order</button>
+    </div>
+  );
 }
