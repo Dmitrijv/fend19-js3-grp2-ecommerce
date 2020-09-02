4b693be7faa517831facda4d885593a9b002835a
Christopher Carleson <christopher.carleson@gmail.com>
2020-09-02 16:46:15 +0200

    Visual effect on add to vart-button, functional.

--- src/components/AddToCartButton.jsx
+++ src/components/AddToCartButton.jsx
@@ -1,27 +1,58 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useRef } from "react";
 import { CartContext } from "../contexts/CartContext";
 import { ProductsContext } from "../contexts/ProductsContext";
 
 export default function AddToCartButton({ productId }) {
   const { cart, setCart } = useContext(CartContext);
   const { products } = useContext(ProductsContext)
-  
+  const thisBtn = useRef()
+
+  function addBtnVisual(buySuccess) {
+
+      thisBtn.current.disabled = true
+      
+      if (buySuccess) {
+      console.log("YAY!" + buySuccess);
+      thisBtn.current.textContent = "Added!"
+      thisBtn.current.classList.add("added-btn")
+      setInterval(function() {
+        thisBtn.current.textContent = "add to cart"
+        thisBtn.current.classList.remove("added-btn")
+        thisBtn.current.disabled = false
+      }, 1500);
+
+    } else {
+      console.log("NAY!" + buySuccess);
+      setInterval(function() {
+        thisBtn.current.textContent = "add to cart"
+        thisBtn.current.className = ""
+        thisBtn.current.disabled = false
+    }, 1500);
+      thisBtn.current.textContent = "Failed to add to cart!"
+      thisBtn.current.className = "failed-btn"
+    }
+  }
   
   const addToCart = () => {
     const productInStock = products[productId].stock // Variable that shows how many of the clicked product in stock
     let updatedCart = {};
 
-    if (productInStock != 0) {
+    let buySuccess = true
+
+    if (productInStock > 0) {
       if (cart[productId]) {
         const buyAmount = cart[productId].qty
         console.log("Hur många av denna produkt finns i varukorgen innan klick: " + buyAmount);
         if (buyAmount >= productInStock) {
+          buySuccess = false
           console.log("Nu vill du köpa fler än vad som finns i lagret!");
         } else {
           updatedCart = { ...cart };
           updatedCart[productId].qty++;
           setCart(updatedCart);
           pushToLocalStorage(updatedCart);
+
+
         }
       } else {
         let newProduct = {
@@ -38,7 +69,10 @@ export default function AddToCartButton({ productId }) {
         setCart(updatedCart);
         pushToLocalStorage(updatedCart);
       }
+    } else {
+      buySuccess = false
     }
+    addBtnVisual(buySuccess)
   };
 
   const pushToLocalStorage = (updatedCart) => {
@@ -46,5 +80,5 @@ export default function AddToCartButton({ productId }) {
     console.log("pushed");
   };
 
-  return <button onClick={() => addToCart()}>add to cart</button>;
+  return <button ref={thisBtn} onClick={() => addToCart()}>add to cart</button>;
 }
