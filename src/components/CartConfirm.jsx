e24f9ee8c38c6caeb259e6c433aa7e1f5d3474a7
Maria Hannesdottir <mariaberghannesdottir@gmail.com>
2020-09-02 13:49:36 +0200

    added handleOnClick

--- src/components/CartConfirm.jsx
+++ src/components/CartConfirm.jsx
@@ -1,19 +1,24 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function CartConfirm() {
+  const [orderData, setOrderData] = useState({});
+
   const firstnameInput = useRef();
   const lastnameInput = useRef();
   const URL = "google.com";
 
-  const orderData = {
-    "group-2": {
-      firstname: firstnameInput.current.value,
-      lastname: lastnameInput.current.value,
-      //hämta från useContext: carten
-      //ordered_products: [item],
-      // total: price,
-    },
-  };
+  function handleOnClick() {
+    setOrderData({
+      "group-2": {
+        //firstname: firstnameInput.current.value,
+        //lastname: lastnameInput.current.value,
+        //hämta från useContext: carten
+        //ordered_products: [item],
+        // total: price,
+      },
+    });
+    // sendOrderToAPI();
+  }
 
   function sendOrderToAPI() {
     //fetch
@@ -25,13 +30,9 @@ export default function CartConfirm() {
 
   return (
     <div>
-      <input ref={firstnameInput} type="text">
-        First name:
-      </input>
-      <input ref={lastnameInput} type="text">
-        Lastname:
-      </input>
-      <button onClick={sendOrderToAPI}>Confirm order</button>
+      <input ref={firstnameInput} type="text" placeholder="First name:" />
+      <input ref={lastnameInput} type="text" placeholder="Last name:" />
+      <button onClick={handleOnClick}>Confirm order</button>
     </div>
   );
 }
