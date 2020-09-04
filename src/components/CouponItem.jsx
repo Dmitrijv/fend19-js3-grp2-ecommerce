fd5816f4f5b7b3b52b534854ef0694235e6bcabb
Dmitrijv <dmitrij.vel@live.se>
2020-09-04 15:02:53 +0200

    created a reusable component for coupons

---
+++ src/components/CouponItem.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CouponItem({ coupon }) {
+  const discountString = (1 - coupon.discount) * 100;
+  return (
+    <div className="coupon">
+      <div className="coupon-intro">
+        <h4>{coupon.campaignName}</h4>
+      </div>
+      <div className="coupon-value">{discountString}%</div>
+    </div>
+  );
+}
