4877f6045ec5a17c5935dd2a9c7a9f6bff59ff1d
Eiln <ebostrom@live.se>
2020-09-07 14:50:28 +0200

    Styling hotfix
    Removed width on body and added headline on cartpage

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -6,6 +6,7 @@ import CartConfirm from "../components/CartConfirm";
 export default function CartPage() {
   return (
     <div className="centered-container cart-page">
+      <h1 className="cart-headline">Let's make an order</h1>
       <div className="white-card">
         <h2 className="page-title">Checkout</h2>
         <CartList />
